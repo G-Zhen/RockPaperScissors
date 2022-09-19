e6d175f31df63e0edda77d0ff8faed8a2e70fd2f
Grace Zhen <grace.zhen00@gmail.com>
2022-09-19 16:27:53 -0700

    Updated README>md and set up the necessary psuedocode for index.js

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ RPS website
 
 This project has 2 stages:
 1. Javascript only
+    * The main focus of this project. Learn to be comfortable with Javascript syntax and the basics of Chrome dev ops tools. 
 2. HTML and CSS 
+    * Website layout and design.
 
 
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-    //return random "Rock", "Paper", "Scissors"
+    //Computer: return random "Rock", "Paper", "Scissors"
     const hand = ["Rock", "Paper", "Scissors"];
     const random = Math.floor(Math.random() * hand.length);
     const r = "Rock";
@@ -11,7 +11,33 @@ function getComputerChoice() {
     } else if (random == 2) {
         return p;
     } else {return s};
+}
+console.log(getComputerChoice())
+
+function playRound(playerSelection, computerSelection) {
+    // Plays 1 round 
+    // User answer (rock, Rock, ROck, etc) will not be case sensitive
+    let state = "Placement";
+    
+    
+    // if player wins
+    // return `You Win. ${playerSelection} beats ${computerSelection}!`
+
+    // if computer wins
+    // return `You Lose. ${computerSelection} beats ${playerSelection}!`
+
+    // if tie 
+    // return `Tie Game. ${playerSelection} is equal to ${computerSelection}!`
+}
+
+function game() {
+    // plays the game for 5 rounds. Keeps score. Announces overall winner (at least 3 wins)
+
+    for (let i = 0; i < 5; i++) {
+        //run playRound()
+        //increment overall wins
+        
+    }
     
 }
 
-console.log(getComputerChoice())
