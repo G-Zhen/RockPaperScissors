6cc6b70b008f629065ea924fcf0dbd71b3225a57
Grace Zhen <grace.zhen00@gmail.com>
2022-09-18 20:09:27 -0700

    finished getComputerChoice() on index.js

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function getComputerChoice() {
     //return random "Rock", "Paper", "Scissors"
-    console.log("Hello")
+    const hand = ["Rock", "Paper", "Scissors"];
+    const random = Math.floor(Math.random() * hand.length);
+    const r = "Rock";
+    const p = "Paper";
+    const s = "Scissors";
+    
+    if (random == 1) {
+        return r;
+    } else if (random == 2) {
+        return p;
+    } else {return s};
+    
 }
 
-console.log("Hello");
+console.log(getComputerChoice())
