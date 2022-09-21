41796e68e98ce62207a648a8e79e054dccb3494c
Grace Zhen <grace.zhen00@gmail.com>
2022-09-20 20:18:03 -0700

    Readjusted playRound to return an array. But game() still doesn't return proper values.

--- index.js
+++ index.js
@@ -9,35 +9,46 @@ function playRound(playerSelection, computerSelection) {
     // Plays 1 round 
     // User answer (rock, Rock, ROck, etc) will not be case sensitive; converts all player inputs into all caps
     let playerText = playerSelection.toUpperCase();
-    
+
     if (playerText == "ROCK" && computerSelection == "SCISSORS" || playerText == "SCISSORS" && computerSelection == "PAPER" || playerText == "PAPER" && computerSelection == "ROCK") {
         // if player wins
-        return `You Win. ${playerText} beats ${computerSelection}!`;
+        return ["You Win!", `You Win. ${playerText} beats ${computerSelection}!`];
     } else if (playerText == "SCISSORS" && computerSelection == "ROCK" || playerText == "PAPER" && computerSelection == "SCISSORS" || playerText == "ROCK" && computerSelection == "PAPER") {
         // if computer wins
-        return `You Lose. ${computerSelection} beats ${playerText}!`;
-    } else {
+        return ["You Lose!", `You Lose. ${playerText} loses to ${computerSelection}!`];
+    } else if (playerText == computerSelection) { 
         // if tie 
-        return `Tie Game. ${playerSelection} is equal to ${computerSelection}!`;
+        return ["Tie Game.", `Tie. ${playerText} is equal to ${computerSelection}!`];
+    } else {
+        // --> null value with no input or not R,p,s --> try again? 
+        return "Improper value.";
     }
+    
 }
 
-function game() {
+function game(playerSelection, computerSelection) {
     // plays the game for 5 rounds. Keeps score. Announces overall winner (at least 3 wins)
+    let playerWins = 0;
+    let compWins = 0;
+
     for (let i = 0; i < 5; i++) {
-        let results = playRound();
-        //increment overall wins
-        let playerWins = 0;
-        let compWins = 0;
-        if (results == `You Win. ${playerText} beats ${computerSelection}!`) {
-            ++playerWins;
-        } else if (results == `You Lose. ${computerSelection} beats ${playerText}!`) {
+        let results = playRound(playerSelection, computerSelection);
+
+        if (results[0] == "You Win!") {
+            ++playerWins;        
+        } else if (results[0] == "You Lose!") {
             ++compWins;
-        }    
+        } 
     }
-    return `Player: ${playerWins} - ${compWins} :Computer`;
+    return [playerWins, compWins];
 }
 
 // Actually start playing
-let user = prompt("Enter your hand");
+let user = prompt("Enter your hand"); //user enters their choice
+
+console.log(playRound(user, getComputerChoice())); // check playRound(p, c); works
+
+console.log('check last function');
+console.log(game(user, getComputerChoice()))
+
 
