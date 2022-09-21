d6d06899bfdfac3cafc90bb27617b8556e59544f
Grace Zhen <grace.zhen00@gmail.com>
2022-09-20 21:35:43 -0700

    Created getPlayerChoice() and will split up playRound() into new function checkIsWinner()

--- index.js
+++ index.js
@@ -1,54 +1,73 @@
+const hand = ["ROCK", "PAPER", "SCISSORS"];
+
 function getComputerChoice() {
     //Computer: return random "Rock", "Paper", "Scissors"
-    const hand = ["ROCK", "PAPER", "SCISSORS"];
     const random = Math.floor(Math.random() * hand.length);
     return hand[random];
 }
 
+function getPlayerChoice() {
+    valid = false
+    // keep input box until valid answer is entered
+    while (valid == false) {
+        const input = prompt("ROCK, PAPER, or SCISSORS? Choose your hand.");
+        if (input == null) {
+            continue;
+        }
+    inputUpper = input.toUpperCase();
+        if (hand.includes(inputUpper) == true) {
+            return inputUpper;
+        }
+    }
+}
+console.log(getPlayerChoice())
+function checkIsWinner() {
+
+}
+
 function playRound(playerSelection, computerSelection) {
     // Plays 1 round 
-    // User answer (rock, Rock, ROck, etc) will not be case sensitive; converts all player inputs into all caps
-    let playerText = playerSelection.toUpperCase();
+    let playerWins = 0;
+    let compWins = 0;
 
-    if (playerText == "ROCK" && computerSelection == "SCISSORS" || playerText == "SCISSORS" && computerSelection == "PAPER" || playerText == "PAPER" && computerSelection == "ROCK") {
+    if (playerSelection == "ROCK" && computerSelection == "SCISSORS" || playerSelection == "SCISSORS" && computerSelection == "PAPER" || playerSelection == "PAPER" && computerSelection == "ROCK") {
         // if player wins
-        return ["You Win!", `You Win. ${playerText} beats ${computerSelection}!`];
-    } else if (playerText == "SCISSORS" && computerSelection == "ROCK" || playerText == "PAPER" && computerSelection == "SCISSORS" || playerText == "ROCK" && computerSelection == "PAPER") {
+        ++playerWins
+        return ["You Win!", `You Win. ${playerSelection} beats ${computerSelection}!`];
+    } else if (playerSelection == "SCISSORS" && computerSelection == "ROCK" || playerSelection == "PAPER" && computerSelection == "SCISSORS" || playerSelection == "ROCK" && computerSelection == "PAPER") {
         // if computer wins
-        return ["You Lose!", `You Lose. ${playerText} loses to ${computerSelection}!`];
-    } else if (playerText == computerSelection) { 
+        ++compWins
+        return ["You Lose!", `You Lose. ${playerSelection} loses to ${computerSelection}!`];
+    } else if (playerSelection == computerSelection) { 
         // if tie 
-        return ["Tie Game.", `Tie. ${playerText} is equal to ${computerSelection}!`];
-    } else {
-        // --> null value with no input or not R,p,s --> try again? 
-        return "Improper value.";
-    }
+        return ["Tie Game.", `Tie. ${playerSelection} is equal to ${computerSelection}!`];
+    } 
     
 }
 
 function game(playerSelection, computerSelection) {
     // plays the game for 5 rounds. Keeps score. Announces overall winner (at least 3 wins)
-    let playerWins = 0;
-    let compWins = 0;
-
+    // why isnt this looping
     for (let i = 0; i < 5; i++) {
-        let results = playRound(playerSelection, computerSelection);
+        console.log("before round")
+        // playRound isnt activating during loop?
+        playRound(playerSelection, computerSelection);
+        console.log("after round")
 
-        if (results[0] == "You Win!") {
-            ++playerWins;        
-        } else if (results[0] == "You Lose!") {
-            ++compWins;
-        } 
+        if (playerWins > compWins) {
+            return 'You have completely beaten the computer';
+        } else if (playerWins < compWins) {
+            return 'You have been anhilated';
+        } else if (playerWins == compWins) {
+            return 'You are equals'
+        }
     }
-    return [playerWins, compWins];
+
+    //why when computer wins 1 round, gains 5 points?
+    //return [playerWins, compWins];
 }
 
 // Actually start playing
-let user = prompt("Enter your hand"); //user enters their choice
 
-console.log(playRound(user, getComputerChoice())); // check playRound(p, c); works
-
-console.log('check last function');
-console.log(game(user, getComputerChoice()))
 
 
